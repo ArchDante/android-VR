917d490c8cf07c3534b8fec4f2457b63d957f221
Dominik Wojtyna <dkfwojtyna@gmail.com>
2017-06-08 18:14:32 +0200

    Add files via upload

---
+++ Scripts/ButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public bool gameStarted = false;
+    public Canvas canvas;
+    public Canvas panel;
+    public bool pressed=false;
+
+    public void NewGameBtn(string newGameLevel)
+    {
+        gameStarted = true;
+        canvas.enabled = false;
+        panel.enabled = false;
+    }
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+}
